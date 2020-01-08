11486dd2058d37af801675e057be418eb7bedcf5
wangjijun <wangjijun1990@126.com>
2020-01-08 09:58:56 +0800

    webpack 热更新

---
+++ src/print.js
@@ -0,0 +1,3 @@
+export default function printMe() {
+  console.log('change other console log11111');
+}
--- webpack.config.js
+++ webpack.config.js
@@ -10,34 +10,39 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ManifestPlugin =  require('webpack-manifest-plugin');
+const webpack = require('webpack');
 // htmlWebpackPlugin 会在打包结束后自动生成一个html文件
 // 打包生成的js自动引入到html中
 // plugin 可以再webpack运行到一个特定的时刻干一件事情
 module.exports = {
   mode: 'development', // 打包模式，会警告，如果不设置，默认生产环境
-  devtool: 'source-map', // sourceMap关闭, sourceMap是映射关系，他知道打包后文件每行代码对应src文件对应文件第几行
+  devtool: 'inline-source-map', // sourceMap关闭, sourceMap是映射关系，他知道打包后文件每行代码对应src文件对应文件第几行
   // 推荐建议 dev =》 cheap-module-eval-source-map pro => cheap-module-source-map
   // cheap为只精确到行 inline为集合到行内文件
   context: path.resolve(__dirname, 'src'),
-  devServer: {
-    contentBase: './dist',  // 服务器启动在哪个文件夹下
-    open: true,  // 自动打开浏览器，自动访问服务器地址
-    port: 8091
+  devServer: { // 本地启动node服务, 本质上为启动服务后打包文件没有放到磁盘，放到了内存中
+   contentBase: './dist',  // 服务器启动在哪个文件夹下 path.join(__dirname, "dist")
+    // open: true,  // 自动打开浏览器，自动访问服务器地址
+    port: 8091,
+    hot: true, // 开启hot module replace功能, 主要是为了保留原来的页面,方便调试页面 ，原则上需要写module.hot代码编写，用到accept方法，
+    hotOnly: true // 如果HMR失效，则就让页面失效，不要刷新页面
   },
   entry: {
     app: './index.js'
   },
   // 简写形式：=》 entry: './src/index.js', // 可以配置多文件入口，数组格式
   output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, 'dist')
-    // publicPath: '/'
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+    // publicPath: '/' 确保提供正确的文件路径
     // 所有输出文件的目标路径
     // __dirname 代表webpack.config.js所在的目录的绝对路径
     // 必须是绝对路径 Node path模块下的__dirname代表当前根目录 ，bundle代表当前目录下的文件夹名称
   },
   // 模块打包配置文件，告诉指定类型文件通过loader来处理成模块, 针对非js文件
   // 如果引入的文件不是js，首先想到用loader来处理
+  // 小型轿车 别克牌SGM7133LBA1
   module: {
     rules: [
       {
@@ -61,7 +66,19 @@ module.exports = {
             id: 'id',
             insert: 'body'
           } } },
-          { loader: 'css-loader' },
+          { loader: 'css-loader' }, 'postcss-loader'
+        ]
+        // css-loader 分析css-loader关系，生成对应的css
+        // style-loader 负责在对应页面挂载css
+      },
+      {
+        test:  /\.scss$/,
+        use: [
+          { loader: 'style-loader', options: { attributes: {
+            id: 'id',
+            insert: 'body'
+          } } },
+          'css-loader', 'sass-loader', 'postcss-loader'
         ]
         // css-loader 分析css-loader关系，生成对应的css
         // style-loader 负责在对应页面挂载css
@@ -94,6 +111,7 @@ module.exports = {
     ]
   },
   plugins: [new HtmlWebpackPlugin({
-    template: './index.html'
-  }), new CleanWebpackPlugin()]
+    template: './index.html',
+    title: 'Development'
+  }), new CleanWebpackPlugin(), new webpack.HotModuleReplacementPlugin()]
 };
